d274f80fd25cedbc28f1f39395a80f35e4d457c0
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2022-09-16 16:19:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,22 @@ resource "local_file" "foo" {
     content     = terraform.workspace
     filename = "${path.module}/sample"
 }
+
+resource "random_integer" "num" {
+  min = 1
+  max = 50000
+}
+
+output "result" {
+  value = random_integer.num.result
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+output "resultstr" {
+  value = random_string.random.result
+}
